460eba26544d7844db9ae9ffbb1a3a8dc774bd99
FPC <felipepalmacastro@gmail.com>
2024-03-21 22:50:32 -0300

    Import module works.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/felipepalma1/lower_order
 
 go 1.22
+
+require github.com/felipepalma1/melina_zero v0.0.0-20240321202010-1a7661169710
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/felipepalma1/melina_zero/auditing_operations"
+	"math"
+)
 
 func main() {
 	fmt.Println("Welcome to Lower Order")
+	fmt.Println(math.Pi)
+	fmt.Println(auditing_operations.DefiningTarget())
 }
